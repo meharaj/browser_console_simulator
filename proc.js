ce56e403e3bbc14688fede2ac01f237f04db50d0
meharaj m <meharaj415@live.in>
2015-06-21 10:37:17 +0530

    basic expressions
    used eval() function to achieve

--- proc.js
+++ proc.js
@@ -2,4 +2,36 @@
 $(document).ready(function () {
 	$("#display_con").click(function (){
 	$("#console").fadeIn('slow');});
+	
+	$("#input").keypress(function(e){
+		if(e.which == 13){
+			string=$("#input").val();
+			output=$("#output").val();
+			
+			
+			for(i in console)
+			{
+				console.log(i)
+			}
+			
+			str_out=eval(string);
+			if(str_out == undefined)
+			{
+				$("#output").val(">"+output+string+"\n");
+				$("#input").val("");
+			}
+			else
+			{
+			
+				string=">"+string+"\n"+str_out+"\n";
+				$("#output").val(output+string+"\n");
+				$("#input").val("");
+			}
+			//$("#output").scrollTop($("#output").scrollHeight -$("#output").height());
+			  $("#output").scrollTop(99999)
+      $("#output").scrollTop($("textarea").scrollTop()*12)
+			console.log("enter");
+		}
+		});
 });
+

e933aab5210de561ef986ed2d551b75d2563871c
meharaj m <meharaj415@live.in>
2015-06-21 23:54:10 +0530

    completely functioning javascript console
    some of the commands have not been handled

--- proc.js
+++ proc.js
@@ -1,31 +1,116 @@
 //javascript
-$(document).ready(function () {
-	$("#display_con").click(function (){
-	$("#console").fadeIn('slow');});
+function Console() {}
+Console.prototype.log=function(par){
+	return par;
+}
+clear=function()
+{
+	return "clr";
 	
-	$("#input").keypress(function(e){
-		if(e.which == 13){
+}
+Console.prototype.clear=clear;
+Console.prototype.error=function(par){
+	return "Error : "+par;
+}
+Console.prototype.warn=function(par){
+	return "Warning ! "+par;
+}
+Console.prototype.assert=function(par1,par2){
+	if(!par1)
+	{
+		return "Assertion failed : "+par2
+	}
+	return "undefined"
+}
+function len(arg)
+{
+	return arg.length;
+}
+$(document).ready(function () {
+	console = new Console();
+	cmds=[];
+	cmds2=[];
+	var flag=1;
+	$("#display_con").click(function (){
+	if(flag){
+	$("#console").slideDown('fast').focus();
+	//$("#console").focus();
+	flag=0;}
+	else
+	{
+	$("#console").slideUp('slow');
+	flag=1;
+	}});
+	
+	$("#input").on("keyup",function(e){
+	
+		
+		index=len(cmds);
+		up=index-1;
+		if(e.which == 38)//up arrow
+		{		
+			cmd=cmds.pop();
+			//alert(cmd);
+			cmds2.push(cmd)
+			$("#input").val(cmd);
+						
+		}
+		if(e.which == 40)
+		{
+			cmd=cmds2.pop()
+			cmds.push(cmd)
+			$("#input").val(cmd);
+		}
+		
+		
+		
+		
+		
+		if(e.which == 13){ //enter key
 			string=$("#input").val();
 			output=$("#output").val();
 			
+			cmds.push(string);
+			console.log(cmds);
 			
-			for(i in console)
+		/*	for(i in console)
 			{
 				console.log(i)
 			}
-			
-			str_out=eval(string);
-			if(str_out == undefined)
+			*/
+			try
 			{
-				$("#output").val(">"+output+string+"\n");
-				$("#input").val("");
+				str_out=eval(string);
+				if(str_out == undefined)
+				{
+					$("#output").val(">"+output+string+"\n");
+					$("#input").val("");
+				}
+				else if(str_out == "clr")
+				{
+					$("#output").val("");
+					$("#input").val("");
+				}
+				/*else if(str_out=="red")
+				{
+					$("#output").style.color="red";
+					$("#output").val(">"+output+string+"\n");
+					$("#input").val("");
+					
+				}*/
+				else
+				{
+				
+					string=">"+string+"\n"+str_out+"\n";
+					$("#output").val(output+string+"\n");
+					$("#input").val("");
+				}
 			}
-			else
+			catch(err)
 			{
-			
-				string=">"+string+"\n"+str_out+"\n";
-				$("#output").val(output+string+"\n");
-				$("#input").val("");
+				$("#output").val(">"+output+string+"\n"+err.message+"\n");
+				
+					$("#input").val("");
 			}
 			//$("#output").scrollTop($("#output").scrollHeight -$("#output").height());
 			  $("#output").scrollTop(99999)
